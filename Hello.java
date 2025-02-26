cd94a62e422a452822eddd33fb83f1b6bb2e179a
P-1997 <164078270+P-1997@users.noreply.github.com>
2025-02-27 00:27:49 +0530

    Update Hello.java -1

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("im  Pradeep");
 		System.out.println(" welcome to world of jenkins ");
+		System.out.println(" webhook is added to get the build when new changes is pushed ");
 	}
 
 }

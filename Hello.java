8a9855b5ea9d9b42f0b5b17b740a3d2f4f13c60b
P-1997 <164078270+P-1997@users.noreply.github.com>
2025-02-26 18:54:29 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 
-public class NewProject {
+public class Hello {
 	public static void main(String[] args) {
 		System.out.println("im  Pradeep");
 		System.out.println(" welcome to world of jenkins ");
